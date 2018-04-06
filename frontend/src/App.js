317e439e6c0ba2cc3a651a64ff3fbf33edfb2ebd
Daniyal Basit Khan <daniyal@gems.techverx.com>
2018-04-07 00:48:10 +0500

    added home link

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Route, Switch, withRouter} from 'react-router-dom';
+import { Route, Switch, withRouter, Link} from 'react-router-dom';
 import CategoryIndex from "./components/CategoryIndex";
 import Header from './components/Header';
 import CategoryLink from './components/CategoryLink';
@@ -19,13 +19,10 @@ class App extends Component {
       <div className="App">
 				<Header heading="Readable App"/>
 				<div className="filters">
-						{ Array.isArray(categories) &&
-								categories.map((category) => 
-								<CategoryLink 
-										key={category.name}
-										name = {category.name}
-								/> 
-						)}
+	        <Link to="/" className="btn btn-category" >Home</Link>				
+					{ Array.isArray(categories) && categories.map((category) => 
+						<CategoryLink key={category.name} name = {category.name} /> 
+					)}
 				</div>
 				{ Array.isArray(categories) && categories.map((category) => 
 					<Switch key={postID()}>
